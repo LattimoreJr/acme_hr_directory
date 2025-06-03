d21c07a9e8ab434dc82d8fa8152ce2cf86935ef1
William Lattimore Jr <lattimorejr@Junior-M4Pro.local>
2025-06-03 00:17:23 -0700

    finished assignment

--- Documents/Coursework/acme_hr_directory/index.js
+++ Documents/Coursework/acme_hr_directory/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const pg = require('pg');
 const client = new pg.Client("postgres://localhost/acme_hr_directory" || process.env.DATABASE_URL);
+app.use(express.json())
 
 app.get('/api/departments', async (req,res,next) => {
     try {
@@ -27,6 +28,48 @@ app.get('/api/employees', async (req,res,next) => {
     } catch (error) {
         next(error)
     }
+})
+
+app.post('/api/employees', async (req, res, next) => {
+  try {
+    const SQL = `
+      INSERT INTO employee(name, department_id)
+      VALUES($1, $2)
+      RETURNING *
+    `
+    const response = await client.query(SQL, [req.body.name, req.body.department_id])
+    res.send(response.rows[0])
+  } catch (ex) {
+    next(ex)
+  }
+})
+
+app.put('/api/employees/:id', async(req,res,next) => {
+        try {
+            const SQL = `
+                UPDATE employee
+                SET department_id = $1
+                WHERE id = $2
+                RETURNING *
+            `
+            const response = await client.query(SQL, [req.body.department_id, req.body.params.id])
+            res.send(response.rows[0])
+        } catch (error) {
+            next(error)
+        }
+})
+
+app.delete('/api/employees/:id', async (req, res, next) => {
+  try {
+    const SQL = `
+      DELETE from employee
+      WHERE id = $1
+    `
+    const response = await client.query(SQL, [req.params.id])
+    res.sendStatus(204)
+  } catch (ex) {
+    next(ex)
+  }
 })
 
 
